9931429d8ebabffcc31865b1ccde402d2f79ec34
Marcos Cordeiro <marcos.cordeiro@cedrotech.com>
2020-04-07 15:53:19 -0300

    Initial commit

---
+++ EzPDF/HtmlRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace EzPDF
+{
+    public class HtmlRenderer
+    {
+        private Browser Browser { get; set; }
+
+        private async Task InitializeBrowser()
+        {
+            if (Browser == null)
+            {
+                await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+                Browser = await Puppeteer.LaunchAsync(new LaunchOptions
+                {
+                    Headless = true
+                });
+            }
+        }
+        
+        public async Task<byte[]> Render(string html)
+        {
+            await InitializeBrowser();
+            var page = await Browser.NewPageAsync();
+            await page.SetContentAsync(html);
+            var pdfData = await page.PdfDataAsync();
+            await page.CloseAsync();
+            return pdfData;
+        }
+    }
+}
